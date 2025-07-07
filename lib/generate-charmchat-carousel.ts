501fd35b174c8a906b7e6d83d5cbc1d6bcb5cce6
Binxu Miao <binxumiao@gmail.com>
2025-07-07 01:04:15 -0700

    revise prompt

--- lib/generate-charmchat-carousel.ts
+++ lib/generate-charmchat-carousel.ts
@@ -45,7 +45,7 @@ export const buildCharmPrompt = (
       replyMessage,
       "Refer the title to write extremely short and extremely concise prompt which can generate these messages"
     )
-    prompt["Tone"] = fallback(tone, "Choose from Dating, Flirty, or Sassy")
+    prompt["Tone"] = fallback(tone, "Choose from Dating, Flirty, Sassy, or NSFW")
 
     for (let i = 1; i <= 3; i++) {
       prompt[`Message ${i}`] = `Fill the blank based on the context`
