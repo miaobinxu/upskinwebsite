81a87fff0ac4111075e477fe699fe101388af829
akshay <akshaypatel959697@gmail.com>
2025-07-07 09:53:10 +0530

    changes

--- components/charmchat/CharmChatPreviewScreen.tsx
+++ components/charmchat/CharmChatPreviewScreen.tsx
@@ -61,7 +61,7 @@ export default function CharmChatPreviewScreen({ images }: CharmChatPreviewScree
 
   return (
     <div className='flex flex-col items-center p-4'>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4 place-items-center w-full">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center w-full">
         {images.map((img, index) => {
           const isFirst = index === 0
           const isLast = index === images.length - 1
