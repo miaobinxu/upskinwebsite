8b9416a4e4b45ccba74518f06e5813fa0fc3c88f
pshiyi <c2shiyi@gmail.com>
2025-10-26 22:05:32 -0700

    Add redirects for product pages

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/products',
+        destination: 'https://charmchat.app/male',
+        permanent: true, // 308 permanent redirect
+      },
+      {
+        source: '/products-es',
+        destination: 'https://charmchat.app/male-es',
+        permanent: true, // 308 permanent redirect
+      },
+    ]
+  },
 }
 
 export default nextConfig
